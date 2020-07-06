0cfb424976bc1b31d8b4c9a62994c1d130b96b34
ITgenie98 <itgenie98@gmail.com>
2020-07-07 01:43:32 +0200

    Bump to version 1.0.0
    this commit actualy holds two versions:
    0.0.2: fixed homepage url and dartdoc, also formated dart source using dartfmt
    1.0.0: First full release, to make pub.dev happy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+## [1.0.0] - 2020-07-07
+
+* First full release
+
+## [0.0.2] - 2020-07-07
+
+* Added homepage URL
+* Formated dart code
+* Try to fix dartdoc crash
+
 ## [0.0.1] - 2020-07-07
 
 * Initial Release
--- lib/better_enum.dart
+++ lib/better_enum.dart
@@ -1,16 +1,23 @@
+/// Library/Package for the 'Enum' baseclass
 library better_enum;
 
 /// The new baseclass for all enum's
 abstract class Enum<T> {
-
+  /// The value that this enum-member is holding
   final T _value;
+
+  /// The optional name of this enum-member, is only used in [toString()]
   final String _name;
   const Enum(this._value, this._name);
-    
+
+  // Returns the holded value
   T get value => _value;
+
+  // Returns the name, maybe null
   String get name => _name;
-  
+
   /// Returns string representation of the enum
+  ///
   /// if [_name] is a non-null string, then its
   /// used in the result, otherwise not
   toString() {
